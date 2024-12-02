ccdeb42a361ef82c16e6a9c65d70ba84040be5dd
Gabriel Almeida <almeidaggabriel93@gmail.com>
2024-12-02 18:25:35 -0300

    UserUpdateDTO

---
+++ tracker/tracker/src/main/java/com/RoomyExpense/tracker/DTO/UserUpdateDTO.java
@@ -0,0 +1,33 @@
+package com.RoomyExpense.tracker.DTO;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserUpdateDTO {
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
+    private String name;
+
+    @Email(message = "Email must be valid")
+    private String email;
+
+    @Pattern(
+            regexp = "\\d{2,4}-\\d{6,8}",
+            message = "Phone number must match the format: AreaCode-Number (e.g., 011-12345678 or 0376-123456)"
+    )
+    private String phoneNumber;
+
+
+    private Long houseId; // Puede ser null si no se actualiza la casa.
+}
+
+
