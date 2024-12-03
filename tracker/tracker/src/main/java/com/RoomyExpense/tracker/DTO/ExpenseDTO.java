e0978254a00d783f793212d5dc755fa1c6bd2c39
Gabriel Almeida <almeidaggabriel93@gmail.com>
2024-12-03 20:37:17 -0300

    ExpenseDto

---
+++ tracker/tracker/src/main/java/com/RoomyExpense/tracker/DTO/ExpenseDTO.java
@@ -0,0 +1,23 @@
+package com.RoomyExpense.tracker.DTO;
+
+
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+public class ExpenseDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private Double amount;
+    private String category; // FIXED o VARIABLE
+    private LocalDate date;
+    private String houseName; // Nombre de la casa asociada al gasto.
+}
+
