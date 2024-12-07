921acc976ffdd34832fbaad3c2952cb38e086161
Gabriel Almeida <almeidaggabriel93@gmail.com>
2024-12-07 19:50:40 -0300

    small fix

--- tracker/tracker/src/main/java/com/RoomyExpense/tracker/controller/PaymentController.java
+++ tracker/tracker/src/main/java/com/RoomyExpense/tracker/controller/PaymentController.java
@@ -20,7 +20,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/payments")
+@RequestMapping("/payments")
 public class PaymentController {
 
     @Autowired
