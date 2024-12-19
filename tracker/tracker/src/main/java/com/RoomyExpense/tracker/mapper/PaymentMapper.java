58eb69a9d755cf5cc1c819635bc6cf4724f45c0b
Gabriel Almeida <almeidaggabriel93@gmail.com>
2024-12-18 21:28:03 -0300

    created payment mapper

---
+++ tracker/tracker/src/main/java/com/RoomyExpense/tracker/mapper/PaymentMapper.java
@@ -0,0 +1,30 @@
+package com.RoomyExpense.tracker.mapper;
+
+import com.RoomyExpense.tracker.DTO.PaymentCreationDTO;
+import com.RoomyExpense.tracker.DTO.PaymentDTO;
+import com.RoomyExpense.tracker.model.Payment;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class PaymentMapper {
+    public Payment toEntity(PaymentCreationDTO paymentCreationDTO){
+        Payment payment = new Payment();
+
+        payment.setAmount(paymentCreationDTO.getAmount());
+        payment.setPaymentDate(LocalDateTime.now().toLocalDate());
+        //expense y user manejado en la logica de servicio.
+
+        return payment;
+    }
+
+    public PaymentDTO toDTO (Payment payment){
+        return new PaymentDTO(
+                payment.getPaymentDate(),
+                payment.getAmount(),
+                payment.getUser().getName(),
+                payment.getExpense().getName()
+        );
+    }
+}
