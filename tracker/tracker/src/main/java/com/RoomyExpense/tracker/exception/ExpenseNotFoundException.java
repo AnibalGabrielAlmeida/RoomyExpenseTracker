bbcff0ca8c89e4af8968fdef690573a0e1f8f3ab
Gabriel Almeida <almeidaggabriel93@gmail.com>
2025-01-03 18:32:37 -0300

    added new exceptions

---
+++ tracker/tracker/src/main/java/com/RoomyExpense/tracker/exception/ExpenseNotFoundException.java
@@ -0,0 +1,7 @@
+package com.RoomyExpense.tracker.exception;
+
+public class ExpenseNotFoundException extends RuntimeException {
+    public ExpenseNotFoundException(String message) {
+        super(message);
+    }
+}
