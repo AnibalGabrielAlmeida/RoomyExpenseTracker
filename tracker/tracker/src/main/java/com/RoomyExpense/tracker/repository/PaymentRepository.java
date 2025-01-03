1869de0035e3faab1ea79390ac76ab61a92b98f1
Gabriel Almeida <almeidaggabriel93@gmail.com>
2025-01-03 16:34:13 -0300

    created findtotalpaymentbyExpenseSplit method

--- tracker/tracker/src/main/java/com/RoomyExpense/tracker/repository/PaymentRepository.java
+++ tracker/tracker/src/main/java/com/RoomyExpense/tracker/repository/PaymentRepository.java
@@ -2,8 +2,13 @@ package com.RoomyExpense.tracker.repository;
 
 import com.RoomyExpense.tracker.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    @Query("SELECT SUM(p.amount) FROM Payment p WHERE p.expenseSplit.id = :expenseSplitId")
+    Double findTotalPaymentsByExpenseSplit(@Param("expenseSplitId") Long expenseSplitId);
 }
